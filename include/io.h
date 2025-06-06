f669a75b01e02a836121e312fcecfdc5b4a116bb
KRONOX-Javier <polaryskernel@gmail.com>
2025-06-06 09:03:25 -0600

    Release v0.2.0: Added advanced PS/2 keyboard driver and input system

---
+++ include/io.h
@@ -0,0 +1,15 @@
+#ifndef KRONOX_IO_H
+#define KRONOX_IO_H
+
+static inline unsigned char inb(unsigned short port) {
+    unsigned char result;
+    __asm__ __volatile__("inb %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+
+static inline void outb(unsigned short port, unsigned char val) {
+    __asm__ __volatile__("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+#endif
+
