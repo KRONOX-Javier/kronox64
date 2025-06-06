f3f8375f5fcbb8dbb3a4e8aee12279ca6b37aca1
KRONOX-Javier <polaryskernel@gmail.com>
2025-06-06 09:00:20 -0600

    New version: Updated By Javier

--- rustcore/version.rs
+++ rustcore/version.rs
@@ -1,4 +1,4 @@
 pub const VERSION_MAJOR: u8 = 0;
 pub const VERSION_MINOR: u8 = 1;
-pub const VERSION_PATCH: u8 = 0;
-pub const VERSION: &str = "0.1.0";
+pub const VERSION_PATCH: u8 = 1;
+pub const VERSION: &str = "0.1.1";
